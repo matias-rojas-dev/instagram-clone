f85cfc27dbb967a634cc21c24f85be319ce0a280
matias-rojas-dev <you@example.com>
2022-11-15 18:14:52 -0300

    Save likes in the localstorage

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,37 +1,55 @@
-import React, { Fragment, useEffect, useRef, useState } from "react";
-import { ImgWrapper, Img, Button, Article } from "./styles";
-import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import React, { Fragment, useEffect, useRef, useState } from 'react'
+import { ImgWrapper, Img, Button, Article } from './styles'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
 const DEFAULT_IMAGE =
-  "https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_hamsters.jpg";
+  'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_hamsters.jpg'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const refElement = useRef(null);
-  const [show, setShow] = useState(false);
-  const [liked, setLiked] = useState(false);
-  console.log(liked);
+  const refElement = useRef(null)
+  const [show, setShow] = useState(false)
+  const key = `like-${id}`
+  const [liked, setLiked] = useState(() => {
+
+    try {
+      const like = window.localStorage.getItem(key)
+      return like
+    } catch (error) {
+      return false
+    }
+  })
+
+  console.log(liked)
   // lazy load in the component
   useEffect(
     function () {
       Promise.resolve(
-        typeof window.IntersectionObserver !== "undefined"
+        typeof window.IntersectionObserver !== 'undefined'
           ? window.IntersectionObserver
-          : import("intersection-observer")
+          : import('intersection-observer')
       ).then(() => {
         const observer = new window.IntersectionObserver(function (entries) {
-          const { isIntersecting } = entries[0];
+          const { isIntersecting } = entries[0]
           if (isIntersecting) {
-            setShow(true);
-            observer.disconnect();
+            setShow(true)
+            observer.disconnect()
           }
-        });
-        observer.observe(refElement.current);
-      });
+        })
+        observer.observe(refElement.current)
+      })
     },
     [refElement]
-  );
+  )
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder
+  const setLocalStorage = (value) => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <Article ref={refElement}>
@@ -43,11 +61,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button onClick={() => setLiked(!liked)}>
+          <Button onClick={() => setLocalStorage(!liked)}>
             <Icon size='32px' /> {likes} likes!
           </Button>
         </Fragment>
       )}
     </Article>
-  );
-};
+  )
+}
